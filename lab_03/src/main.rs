c0d355cc744ff35e6fcf75540a4de803c5013532
EduMenges <eduardo.mattje@gmail.com>
2023-01-23 09:30:17 -0300

    Copied from the book, not working

--- lab_03/src/main.rs
+++ lab_03/src/main.rs
@@ -1,9 +1,17 @@
 use radix_sort::*;
 
 fn main() {
-    let mut test: Vec<String> = vec!["dab".to_string(), "add".to_string(), "cab".to_string(), "fad".to_string(), "fee".to_string(), "bad".to_string(), "dad".to_string()];
+    let mut test: Vec<String> = vec![
+        "dab".to_string(),
+        "add".to_string(),
+        "cab".to_string(),
+        "fad".to_string(),
+        "fee".to_string(),
+        "bad".to_string(),
+        "dad".to_string(),
+    ];
 
     radix_sort(&mut test);
-    
+
     println!("Hello, world!");
 }
--- radix_sort/src/lib.rs
+++ radix_sort/src/lib.rs
@@ -1,25 +1,65 @@
 mod the_algos;
 
-fn sort(src: &mut[String], lo: usize, hi: usize, pos: usize) {
-    let mut aux: Vec<String> = Vec::with_capacity(hi - lo);
-    let mut count = [0_u16; 258];
-    let char_at = |word: &str, n: usize| -> Option<char> {word.chars().nth(n)};
-    
-    // Computes the frequency count
-    for i in lo..=hi {
-        count[char_at(&src[i], pos + 2).unwrap() as usize] += 1;
-    }
+struct msd_sort {
+    radix: usize,
+    cutoff: usize,
+    aux: Vec<String>,
+}
 
-    // Transform the counts to indices
-    for r in 1..count.len() {
-        count[r] += count[r - 1];
-    }
-
-    // Distribute
-    for i in lo..=hi {
-        
+impl Default for msd_sort {
+    fn default() -> Self {
+        Self {
+            radix: 256,
+            cutoff: 15,
+            aux: Default::default(),
+        }
     }
 }
 
-pub fn radix_sort(src: &mut[String]) {
+impl msd_sort {
+    pub fn sort(&mut self, src: &mut [String]) {
+        self.aux = Vec::with_capacity(src.len());
+        self.sort_(src, 0, src.len() - 1, 0);
+    }
+
+    fn sort_(&mut self, src: &mut [String], lo: usize, hi: usize, pos: usize) {
+        if hi <= lo + self.cutoff {
+            src[lo..hi].sort();
+        }
+
+        let mut count: Vec<usize> = Vec::with_capacity(self.radix + 2);
+        let char_at = |word: &str, n: usize| -> Option<char> { word.chars().nth(n) };
+
+        // Computes the frequency count
+        for i in lo..=hi {
+            count[char_at(&src[i], pos + 2).unwrap() as usize] += 1;
+        }
+
+        // Transform the counts to indices
+        for r in 1..count.len() {
+            count[r] += count[r - 1];
+        }
+
+        // Distribute
+        for i in lo..=hi {
+            let index = char_at(&src[i], pos).unwrap() as usize + 1;
+            count[index] += 1;
+            self.aux[index] = std::mem::replace(&mut src[i], String::default());
+        }
+
+        // Copy back
+        for i in lo..=hi {
+            src[i] = std::mem::replace(&mut self.aux[i - lo], String::default());
+        }
+
+        // Makes the final recursion call
+        for r in 0..self.radix {
+            self.sort_(src, lo + count[r], lo + count[r + 1] - 1, pos + 1);
+        }
+    }
+}
+
+pub fn radix_sort(src: &mut [String]) {
+    let mut msd_struct = msd_sort::default();
+    msd_struct.sort(src);
 }
