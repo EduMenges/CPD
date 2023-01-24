d10fb8ea5c24a32bf801ad8381b2a0e29fca1286
EduMenges <eduardo.mattje@gmail.com>
2023-01-24 11:33:56 -0300

    Added reading from stdin

--- lab_03/src/main.rs
+++ lab_03/src/main.rs
@@ -1,22 +1,36 @@
+use std::io;
+
 use radix_sort::*;
 
 fn main() {
-    let mut test: Vec<String> = vec![
-        "dab".to_string(),
-        "add".to_string(),
-        "cab".to_string(),
-        "fad".to_string(),
-        "fee".to_string(),
-        "bad".to_string(),
-        "dad".to_string(),
-        "bee".to_string(),
-        "fed".to_string(),
-        "bed".to_string(),
-        "ebb".to_string(),
-        "ace".to_string(),
-    ];
+    let mut str_vec: Vec<String> = vec![];
+    // let mut test: Vec<String> = vec![
+    //     "dab".to_string(),
+    //     "add".to_string(),
+    //     "cab".to_string(),
+    //     "fad".to_string(),
+    //     "fee".to_string(),
+    //     "bad".to_string(),
+    //     "dad".to_string(),
+    //     "bee".to_string(),
+    //     "fed".to_string(),
+    //     "bed".to_string(),
+    //     "ebb".to_string(),
+    //     "ace".to_string(),
+    // ];
 
-    radix_sort(&mut test);
+    for line in io::stdin().lines() {
+        match line {
+            Ok(line) => {
+                if line.len() >= 4 {
+                    str_vec.push(line);
+                }
+            }
+            Err(err) => println!("{err}"),
+        }
+    }
 
-    println!("{:?}", test);
+    radix_sort(&mut str_vec);
+
+    println!("{:?}", &str_vec[0..10]);
 }
