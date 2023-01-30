d45f30b6c41e13fd43ae89f017467bcf6de81b7c
EduMenges <eduardo.mattje@gmail.com>
2023-01-30 19:20:59 -0300

    Almost done?

--- lab_03/src/main.rs
+++ lab_03/src/main.rs
@@ -3,36 +3,40 @@ use std::io;
 use radix_sort::*;
 
 fn main() {
-    let mut str_vec: Vec<String> = vec![];
-    // let mut test: Vec<String> = vec![
-    //     "dab".to_string(),
-    //     "add".to_string(),
-    //     "cab".to_string(),
-    //     "fad".to_string(),
-    //     "fee".to_string(),
-    //     "bad".to_string(),
-    //     "dad".to_string(),
-    //     "bee".to_string(),
-    //     "fed".to_string(),
-    //     "bed".to_string(),
-    //     "ebb".to_string(),
-    //     "ace".to_string(),
-    // ];
+    // let mut str_vec: Vec<String> = Vec::new();
+    //
+    // for line in io::stdin().lines() {
+    //     match line {
+    //         Ok(line) => {
+    //             for word in line.split_whitespace() {
+    //                 if word.len() >= 4 {
+    //                     str_vec.push(word.to_owned());
+    //                 }
+    //             }
+    //         }
+    //         Err(err) => println!("{err}"),
+    //     }
+    // }
 
-    for line in io::stdin().lines() {
-        match line {
-            Ok(line) => {
-                for word in line.split(' ') {
-                    if word.len() >= 4 {
-                        str_vec.push(word.to_owned());
-                    }
-                }
-            }
-            Err(err) => println!("{err}"),
-        }
-    }
+    let mut str_vec: Vec<String> = vec![
+        "dab".to_owned(),
+        "addb".to_owned(),
+        "cabcf".to_owned(),
+        "fadde".to_owned(),
+        "feee".to_owned(),
+        "badf".to_owned(),
+        "adb".to_owned(),
+        "dada".to_owned(),
+        "beeba".to_owned(),
+        "fedc".to_owned(),
+        "bedd".to_owned(),
+        "ebb".to_owned(),
+        "acef".to_owned(),
+        "baee".to_owned(),
+        "baeb".to_owned(),
+    ];
 
     radix_sort(&mut str_vec);
 
-    println!("{:?}", &str_vec[0..10]);
+    println!("{:?}", &str_vec);
 }
--- radix_sort/src/lib.rs
+++ radix_sort/src/lib.rs
@@ -1,12 +1,10 @@
-mod the_algos;
-
-struct MsdSort {
+struct MSD {
     radix: usize,
-    cutoff: usize,
+    cutoff: u8,
     aux: Vec<String>,
 }
 
-impl Default for MsdSort {
+impl Default for MSD {
     fn default() -> Self {
         Self {
             radix: 256,
@@ -16,56 +14,56 @@ impl Default for MsdSort {
     }
 }
 
-impl MsdSort {
-    pub fn sort(&mut self, src: &mut [String]) {
-        self.aux = vec![String::default(); src.len()];
-        self.sort_(src, 0, src.len() as isize - 1, 0);
+impl MSD {
+    pub fn char_at(src: &str, pos: usize) -> usize {
+        match src.chars().nth(pos) {
+            Some(ch) => ch as usize,
+            None => 0,
+        }
     }
 
-    fn sort_(&mut self, src: &mut [String], lo: usize, hi: isize, pos: usize) {
-        if hi <= (lo + self.cutoff) as isize {
+    pub fn sort(&mut self, src: &mut [String]) {
+        self.aux = Vec::with_capacity(src.len());
+        self.sort_(src, 0);
+    }
+
+    fn sort_(&mut self, src: &mut [String], pos: usize) {
+        // Cutoff the small subarrays
+        if src.len() < self.cutoff as usize {
             src.sort();
             return;
         }
 
-        let mut count: Vec<usize> = vec![0; self.radix + 2];
-        let char_at = |word: &str, n: usize| word.chars().nth(n).unwrap();
-
-        // Computes the frequency count
-        for i in lo..=hi as usize {
-            count[char_at(&src[i], pos) as usize + 2] += 1;
-        }
-
-        // Transform the counts to indices
-        for r in 0..self.radix + 1 {
-            count[r + 1] += count[r];
-        }
-
-        // Distribute
-        for i in lo..=hi as usize {
-            let index = char_at(&src[i], pos) as usize + 1;
-            self.aux[count[index]] = std::mem::replace(&mut src[i], String::default());
+        // Compute the frequency counts
+        let mut count = vec![0; self.radix];
+        for element in src.as_ref() {
+            let index = Self::char_at(element, pos);
             count[index] += 1;
         }
 
-        // Copy back
-        for i in lo..=hi as usize {
-            src[i] = std::mem::replace(&mut self.aux[i - lo], String::default());
+        // Perform the accumulation
+        let mut accumulation = vec![0; self.radix];
+        for i in 1..accumulation.len() {
+            accumulation[i] += accumulation[i - 1];
         }
 
-        // Makes the final recursion call
-        for r in 0..self.radix {
-            self.sort_(
-                src,
-                lo + count[r],
-                (lo + count[r + 1]) as isize - 1,
-                pos + 1,
-            );
+        // Move to the aux, sorted
+        for i in (0..src.len()).rev() {
+            let ch = Self::char_at(&src[i], pos);
+            accumulation[ch] -= 1;
+            let new_i = accumulation[ch];
+
+            std::mem::swap(&mut src[i], &mut self.aux[new_i]);
+        }
+
+        // Move back to the src
+        for i in 0..src.len() {
+            std::mem::swap(&mut src[i], &mut self.aux[i]);
+        }
+
+        // Recursively sort for the rest of characters
+        for i in 1..self.radix {
+            Self::sort_(&mut self, src, pos + 1);
         }
     }
-}
-
-pub fn radix_sort(src: &mut [String]) {
-    let mut msd_struct = MsdSort::default();
-    msd_struct.sort(src);
 }
