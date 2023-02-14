df42d2b250a38ee2d932fc33dfceafb3e308a21e
EduMenges <eduardo.mattje@gmail.com>
2023-02-14 11:39:10 -0300

    Started lab 04, search needs implementation

---
+++ hash_table/src/lib.rs
@@ -0,0 +1,35 @@
+
+static const P = 123;
+
+pub struct HashTable {
+    table: Vec<LinkedList<String>>,
+}
+
+impl HashTable {
+    pub new(entries: usize) -> Self {
+        table: Vec::with_capacity(entries),
+    }
+
+    pub insert(&mut self, string: String) {
+        let hashed = self.hash(&string);
+        self.table[hashed].push(string);
+    }
+
+    pub search(&self, string: &str) {
+        let hashed = self.hash(string);
+        let list = self.table[hashed];
+        
+        
+    }
+
+    hash(&self, string: &str) -> usize {
+        let m = self.table.len();
+                let mut hash = 0_size;
+
+        for ch in string.chars().rev() {
+            hash = (ch as usize) % m + (P * hash) % m;
+        }
+
+        hash
+    }
+}
