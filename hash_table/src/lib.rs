0b665376963be55a9a4ccb24b29f54e169fdc499
Eduardo Mattje <eduardomattje@gmail.com>
2023-02-15 22:22:59 -0300

    Is working!

--- hash_table/src/lib.rs
+++ hash_table/src/lib.rs
@@ -1,35 +1,113 @@
+#![feature(let_chains)]
+#![feature(exact_size_is_empty)]
 
-use std::collections::LinkedList;
+use std::{collections::LinkedList, fmt::Display};
 
-const P: u8 = 123;
+const P: usize = 257;
 
 pub struct HashTable {
     table: Vec<LinkedList<String>>,
 }
 
+pub struct ListStats {
+    used_entries: usize,
+    empty_entries: usize,
+    occupy_rate: f64,
+    min: usize,
+    max: usize,
+    avg: f64,
+}
+
+impl Display for ListStats {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "NUMERO DE ENTRADAS DA TABELA USADAS {}
+NÚMERO DE ENTRADAS DA TABELA VAZIAS {}
+TAXA DE OCUPACAO {:.2}
+MINIMO TAMANHO DE LISTA {}
+MAXIMO TAMANHO DE LISTA {}
+MEDIO TAMANHO DE LISTA {:.2}",
+            self.used_entries, self.empty_entries, self.occupy_rate, self.min, self.max, self.avg
+        )
+    }
+}
+
 impl HashTable {
-    pub new(entries: usize) -> Self {
-        Self {table: Vec::with_capacity(entries),}
+    pub fn new(entries: usize) -> Self {
+        Self {
+            table: vec![LinkedList::new(); entries],
+        }
     }
 
-    pub insert(&mut self, string: String) {
+    pub fn insert(&mut self, string: String) {
         let hashed = self.hash(&string);
-        self.table[hashed].push(string);
+        self.table[hashed].push_back(string);
     }
 
-    pub search(&self, string: &str) {
+    pub fn search(&self, string: &str) -> i16 {
         let hashed = self.hash(string);
-        let list = self.table[hashed];
-        
-        
+        let list = &self.table[hashed];
+        let mut res = 1;
+        let mut it = list.iter();
+
+        while let Some(element) = it.next() && *element != string {
+            res += 1;
+        }
+
+        if it.is_empty() {
+            res = 0;
+        }
+
+        res
     }
 
-    hash(&self, string: &str) -> usize {
+    #[inline]
+    pub fn get_empties(&self) -> usize {
+        self.table.iter().filter(|l| l.is_empty()).count()
+    }
+
+    #[inline]
+    pub fn get_used(&self) -> usize {
+        self.table.iter().filter(|l| !l.is_empty()).count()
+    }
+
+    pub fn get_list_statistics(&self) -> ListStats {
+        let used_entries = self.get_used();
+        let empty_entries = self.get_empties();
+        let occupy_rate = used_entries as f64 / self.table.capacity() as f64;
+
+        let mut min = self.table[0].len();
+        let mut max = self.table[0].len();
+        let mut avg = 0.0;
+        let total_lists = used_entries as f64;
+
+        self.table.iter().filter(|l| !l.is_empty()).for_each(|l| {
+            if l.len() < min {
+                min = l.len();
+            }
+            if l.len() > max {
+                max = l.len();
+            }
+            avg += l.len() as f64 / total_lists
+        });
+
+        ListStats {
+            min,
+            max,
+            avg,
+            used_entries,
+            empty_entries,
+            occupy_rate,
+        }
+    }
+
+    fn hash(&self, string: &str) -> usize {
         let m = self.table.len();
-                let mut hash = 0_size;
+        let mut hash = 0_usize;
 
         for ch in string.chars() {
-            hash = (ch as usize) % m + (P * hash) % m;
+            hash = ((ch as usize) % m + (P * hash) % m) % m;
         }
 
         hash
--- lab_04/src/main.rs
+++ lab_04/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+use hash_table::HashTable;
+
+const ENTRIES: usize = 503;
+
+fn main() -> io::Result<()> {
+    let nomes_p = Path::new(".\\nomes_10000.txt");
+    let nomes_buf = BufReader::new(File::open(nomes_p)?);
+    let mut nomes_ht = HashTable::new(ENTRIES);
+
+    for s in nomes_buf.lines() {
+        nomes_ht.insert(s?);
+    }
+
+    let consultas = Path::new(".\\consultas.txt");
+    let mut consultas = parse_file(consultas)?;
+
+    println!("{}", nomes_ht.get_list_statistics());
+
+    Ok(())
+}
+
+fn parse_file<P>(path: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let mut res: Vec<String> = Vec::new();
+    let file = File::open(path)?;
+    let buffer = BufReader::new(file);
+
+    for l in buffer.lines() {
+        match l {
+            Ok(l) => res.push(l),
+            Err(e) => return Err(e),
+        }
+    }
+
+    Ok(res)
 }
